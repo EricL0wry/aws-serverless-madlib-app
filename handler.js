e1609e605de06981d49b092f77b436bae4fc76e9
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-28 16:36:04 -0700

    Added some short code to test s3.getObject

--- handler.js
+++ handler.js
@@ -1,9 +1,23 @@
 'use strict';
 
-// const AWS = require('aws-sdk');
-// AWS.config.update({ region: 'us-east-1' });
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+const s3 = new AWS.S3();
 // const ddb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.updateDynamoDb = (event, context, callback) => {
   console.log('Ok');
+  const bucketName = 'cereal-bucket'; // Name of Bucket
+  const keyName = 'shortCereals.json'; // File Name in Bucket
+  const params = { Bucket: bucketName, Key: keyName };
+
+  s3.getObject(params, (err, data) => {
+
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.Body.toString());
+    }
+
+  });
 };
