313c8e70f46437c7c5a28112ecd12ede3fedaa00
Eric Lowry <ericmichaellowry@gmail.com>
2020-05-29 11:34:36 -0700

    Additional adjustments to code. Lambda code is now in working form. Will revisit to remove console logs and use variabls for bucket and key names

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 const s3 = new AWS.S3();
-// const ddb = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.updateDynamoDb = (event, context, callback) => {
   console.log('Ok');
@@ -12,12 +12,34 @@ module.exports.updateDynamoDb = (event, context, callback) => {
   const params = { Bucket: bucketName, Key: keyName };
 
   s3.getObject(params, (err, data) => {
+    let cereals;
 
     if (err) {
       console.log(err);
     } else {
-      console.log(data.Body.toString());
+      cereals = JSON.parse(data.Body);
+
+      for (let i = 0; i < cereals.length; i++) {
+        const cereal = cereals[i];
+        console.log(cereal);
+        addItem(cereal);
+      }
     }
 
+  });
+};
+
+const addItem = itemObj => {
+  const params = {
+    TableName: 'cerealTable',
+    Item: itemObj
+  };
+
+  ddb.put(params, (err, data) => {
+    if (err) {
+      console.error('Unable to add item. Error:', JSON.stringify(err, null, 2));
+    } else {
+      console.log(`Item ${params.Item.name} added!`);
+    }
   });
 };
