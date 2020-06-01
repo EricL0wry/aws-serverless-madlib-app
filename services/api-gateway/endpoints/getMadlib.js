b362c3b47178665b683f76cc7d5e14ab640862c1
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 10:39:15 -0700

    Completed getMadLib functionality to return a single item, along with error handling

--- services/api-gateway/endpoints/getMadlib.js
+++ services/api-gateway/endpoints/getMadlib.js
@@ -7,11 +7,12 @@ const Response = require('../lib/response');
 
 module.exports.getMadlib = async event => {
 
-  if (!event.pathParameters || !event.pathParameters.ID) {
-    return Response({ message: 'Please supply a valid ID' }, 400);
+  const itemId = parseInt(event.pathParameters.ID, 10);
+
+  if (!itemId) {
+    return Response({ message: 'Please enter a valid MadLib ID' }, 400);
   }
 
-  const itemId = parseInt(event.pathParameters.ID, 10);
   const table = process.env.table;
   const params = {
     TableName: table,
@@ -19,60 +20,14 @@ module.exports.getMadlib = async event => {
   };
 
   try {
-    const item = await ddb.get(params).promise();
-    const response = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Origin': '*'
-      },
-      statusCode: 200,
-      body: JSON.stringify({ item })
-    };
-    return response;
+    const madLib = await ddb.get(params).promise();
+    if (!madLib.Item) {
+      return Response({ message: `Unable to locate MadLib with ID ${itemId}` });
+    }
+    return Response(madLib.Item, 200);
   } catch (err) {
-    return {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Origin': '*'
-      },
-      statusCode: 500,
-      body: JSON.stringify(err)
-    };
+    console.error(`There was an error getting ID ${itemId} from table`, err);
+    return Response({ message: 'There was an unexpected error retrieving your item' });
+
   }
-  //  getItem(params).then(data => {
-  //     console.log(data.Item);
-  //     if (!data.Item) {
-  //       return Response({ message: 'Unable to locate item' }, 404);
-  //     } else {
-  //       return Response(data.Item, 200);
-  //     }
-  //   })
-  //     .catch(err => {
-  //       console.error(`There was an error retrieving item ${itemId}`, err);
-  //       return Response({ message: `There was an error retrieving item ${itemId}` }, 400);
-  //     });
-
-  // const item = await ddb.get(params, (err, data) => {
-
-  //   if (err) {
-  //     console.error('An unexpected error has occurred');
-  //     return null;
-  //   }
-
-  //   if (!data || !data.item) {
-  //     console.error(`Unable to locate item for ID ${itemId}`);
-  //     return null;
-  //   }
-
-  //   return data.Item;
-  // });
-
-  // if (!item) {
-  //   return Response({ message: `Unable to locate item for ID ${itemId}` });
-  // } else {
-  //   return Response({ item }, 200);
-  // }
-
 };
