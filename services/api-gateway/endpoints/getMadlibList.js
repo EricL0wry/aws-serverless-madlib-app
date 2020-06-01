e7565ab7ecba244b5d1ea01bb642fa646a42c499
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 11:07:01 -0700

    Created basic code to scan madliblist table

--- services/api-gateway/endpoints/getMadlibList.js
+++ services/api-gateway/endpoints/getMadlibList.js
@@ -6,5 +6,18 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 const Response = require('../lib/response');
 
 module.exports.getMadlibList = async event => {
-  return Response({ message: 'test response' }, 200);
+  // return Response({ message: 'test response' }, 200);
+
+  const table = process.env.table;
+  const params = {
+    TableName: table,
+    ProjectionExpression: 'madLibId, name'
+  };
+
+  try {
+    const madLibList = await ddb.scan(params).promise();
+    return madLibList;
+  } catch (err) {
+    console.error('There was an error getting a list of Madlibs', err);
+  }
 };
