048545b06732dff20e2de9124d9fef3e62e5ece5
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-01 15:10:19 -0700

    Added POST endpoint. All necessary endpoints are working with reasonable error handling in place

--- services/api-gateway/endpoints/getMadlib.js
+++ services/api-gateway/endpoints/getMadlib.js
@@ -23,8 +23,23 @@ module.exports.getMadlib = async event => {
     const madLib = await ddb.get(params).promise();
     if (!madLib.Item) {
       return Response({ message: `Unable to locate MadLib with ID ${itemId}` }, 404);
+    } else {
+      const { madLibId, madLibName } = madLib.Item;
+      const responseObj = {};
+      const wordList = {};
+      responseObj.madLibId = madLibId;
+      responseObj.madLibName = madLibName;
+
+      for (const key in madLib.Item) {
+        if (key.includes('word')) {
+          wordList[key] = madLib.Item[key];
+        }
+      }
+
+      responseObj.words = wordList;
+
+      return Response(responseObj, 200);
     }
-    return Response(madLib.Item, 200);
   } catch (err) {
     console.error(`There was an error getting ID ${itemId} from table`, err);
     return Response({ message: 'There was an unexpected error retrieving your item' }, 500);
--- services/api-gateway/endpoints/postMadlibSubmission.js
+++ services/api-gateway/endpoints/postMadlibSubmission.js
@@ -5,6 +5,24 @@ AWS.config.update({ region: process.env.region });
 const ddb = new AWS.DynamoDB.DocumentClient();
 const Response = require('../lib/response');
 
-module.exports.getMadlib = async event => {
-  return Response({ message: 'Test Response' }, 200);
+module.exports.postMadlibSubmission = async event => {
+
+  const madlib = JSON.parse(event.body);
+  const pin = Math.floor(1000 + Math.random() * 9000);
+  const table = process.env.table;
+  madlib.userPin = pin;
+
+  const params = {
+    TableName: table,
+    Item: madlib
+  };
+
+  try {
+    await ddb.put(params).promise();
+    return Response({ status: 'Success', userPin: pin });
+  } catch (err) {
+    console.error('There was an error submitting a user madlib', err);
+    return Response({ message: 'There was an unexpected error submitting your request' }, 500);
+  }
+
 };
